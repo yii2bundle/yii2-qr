e4ed9dfee18594945564d994740c31228d5e44b0
Vitaliy <theyamshikov@yandex.ru>
2019-01-12 15:05:08 +0600

    refacto

--- src/domain/repositories/ar/CacheRepository.php
+++ src/domain/repositories/ar/CacheRepository.php
@@ -3,14 +3,14 @@
 namespace yii2lab\qr\domain\repositories\ar;
 
 use yii2lab\domain\interfaces\repositories\CrudInterface;
+use yii2lab\extension\activeRecord\repositories\base\BaseActiveArRepository;
 use yii2lab\qr\domain\entities\QrEntity;
 use yii2lab\domain\BaseEntity;
-use yii2lab\domain\repositories\ActiveArRepository;
 use Yii;
 use yii\db\ActiveRecord;
 use yii2lab\qr\domain\interfaces\repositories\CacheInterface;
 
-class CacheRepository extends ActiveArRepository implements CrudInterface, CacheInterface {
+class CacheRepository extends BaseActiveArRepository implements CrudInterface, CacheInterface {
 	
 	protected $primaryKey = 'hash';
 	
